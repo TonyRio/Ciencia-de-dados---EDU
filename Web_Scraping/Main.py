1f7f194160490ba9c547bb9e46e446767c4dc342
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-10-26 14:36:23 -0300

    Update Main.py

--- Web_Scraping/Main.py
+++ Web_Scraping/Main.py
@@ -1,5 +1,10 @@
-from gazpacho import get
+from gazpacho import get, Soup
+
 
 url = 'https://pypi.org/project/pandas/#history'
 html =get(url)
-print(html)
+Soup(html)
+soup = Soup(html)
+soup.find('a', {'card': 'card'})
+cards = soup.find("a", {"class": "card"})
+print(cards[1])
